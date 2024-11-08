7790bc8f0b83aa4632fae78a45b870c1c14a99f0
choihooo <hochoi8621@naver.com>
2024-11-08 16:39:03 +0900

    ranking 페이지 생성

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 function NavBar() {
+  const navigate = useNavigate();
+
   return (
     <div className="h-[70px] w-full bottom-0 bg-[#4a688d] fixed flex justify-evenly items-center text-white">
-      <button className="w-[70px]">랭킹</button>
+      <button className="w-[70px]" onClick={() => navigate("/ranking")}>
+        랭킹
+      </button>
       <div className="h-[50px] w-[1px] bg-white"></div>
-      <button className="w-[70px]">쓰레기 지도</button>
+      <button className="w-[70px]" onClick={() => navigate("/map")}>
+        쓰레기 지도
+      </button>
       <div className="h-[50px] w-[1px] bg-white"></div>
-      <button className="w-[70px]">포인트</button>
+      <button className="w-[70px]" onClick={() => navigate("/point")}>
+        포인트
+      </button>
     </div>
   );
 }
--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,17 +1,22 @@
 import NavBar from "../../components/NavBar";
+import { useNavigate } from "react-router-dom";
 
 function Upload() {
+  const navigate = useNavigate();
   return (
     <div className="w-full h-[100vh] bg-[#e7f0f1] flex flex-col items-center">
       <div className="bg-[#4a688d] fixed top-0 w-full h-[70px] z-10 flex justify-center items-center text-[36px] text-white">
         쓰레기 사진 업로드
       </div>
-      <div className="fixed bottom-[70px] flex flex-col">
-        <img src="/vite.svg" className="mb-[60px]" />
-        <button className="bg-[#88ccf1] mb-[40px] px-[100px] py-[100px] rounded-[100%] text-[20px] text-white">
+      <div className="fixed bottom-[70px] flex flex-col items-center">
+        <img src="/image.png" className="mb-[60px] w-[270px] h-[300px]" />
+        <button className="bg-[#88ccf1] mb-[40px] px-[0px] py-[100px] rounded-[100%] text-[20px] text-white">
           사진 추가
         </button>
-        <button className="bg-[#88ccf1] mb-[40px] px-[100px] py-[16px] rounded-[30px] text-[20px] text-white">
+        <button
+          className="bg-[#88ccf1] mb-[40px] px-[100px] py-[16px] rounded-[30px] text-[20px] text-white"
+          onClick={() => navigate("/map")}
+        >
           제출
         </button>
       </div>
